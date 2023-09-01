e760cfdd2091b2ee33384d66e8b239f4b72e997a
Michiel M <michiel.meire@hotmail.com>
2023-09-01 08:36:13 +0200

    update for test_database

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,11 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# db_test = SQLAlchemy()                                            #todo TEST DATABASE, setup sqlalchemy db connection
+
 # DATABASE URL als variable. Zo kan ik later de url aanpassen naar een microsoft sql
 DATABASE_URL = os.environ.get("DATABASE_URL") or f"sqlite:///{basedir}/{DB_NAME}"
-
+#DATABASE_TEST_URL =                                             #todo TEST DATABASE
 
 # maken van de app, deze wordt opgeroepen in de main.py file in de directory MER-data-app
 def create_app():
@@ -22,6 +24,7 @@ def create_app():
     boostrap.init_app(app)
     app.config["SECRET_KEY"] = "MerdataAppMichiel"
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    # app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL          #todo TEST DATABASE
     db.init_app(app)
 
     from .views import views
@@ -32,6 +35,7 @@ def create_app():
     # app.register_blueprint(merdata, url_prefix="/")
 
     from .models import Material
+    # from .models import Material_test                             #todo TEST DATABASE
 
     with app.app_context():
         db.create_all()
