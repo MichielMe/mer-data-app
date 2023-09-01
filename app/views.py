4c33055e9f68638ff779f56186e87dd4d78fb6e4
Michiel M <michiel.meire@hotmail.com>
2023-09-01 12:43:49 +0200

    update

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ views = Blueprint("views", __name__)
 @views.route("/", methods=["GET", "POST"])
 def index():
     form = UploadForm()
+    material_ids_to_display = []
     if form.validate_on_submit():
         # 1. Read the uploaded Excel file
         file = form.file.data
@@ -17,11 +18,14 @@ def index():
         material_ids = df.iloc[
             :, 0
         ].values.tolist()  # Als de ID's in de eerste column staan.
-
+     
         # 2. Filter the database based on MATERIAL IDs
         materials_to_update = Material.query.filter(
             Material.material_id.in_(material_ids)
         ).all()
+        
+        # Populate the list of material_ids to be displayed on the web page
+        material_ids_to_display = [material.material_id for material in materials_to_update]
 
         # 3. Modify the title and material type columns
         replace_text = form.replace_text.data
@@ -35,14 +39,14 @@ def index():
                 material.title += " " + suffix
 
             material.material_type = material_type
-
+        
         # 4. Commit changes to the database
         db.session.commit()
 
         flash("Database updated successfully!", "success")
         return redirect(url_for("views.index"))
 
-    return render_template("index.html", form=form)
+    return render_template("index.html", form=form, material_ids=material_ids_to_display)
 
 
 
